5fd14ac56e24e71705207404a7d91e32bbf8e75e
iprincipe <56736744+iprincipe@users.noreply.github.com>
2021-08-27 14:56:28 -0500

    Add files via upload

---
+++ _test_/app.test.js
@@ -0,0 +1,6 @@
+import { generatePage } from "../src/client/js/app"
+
+describe("Testing the generate function", () => {  
+    test("Testing the generatePage() function", () => {
+           expect(generatePage).toBeDefined();
+})});
